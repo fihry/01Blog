c0f0d191ac85d479078272f3c0b2b1561e5a4390
MOHAMED EL FIHRY <fihrylara@gmail.com>
2025-12-28 12:23:22 +0100

    feat :adding the comment service and remove the duplicated taost service

---
+++ frontend/src/app/core/services/comment.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export interface Comment {
+    id: string;
+    postId: string;
+    content: string;
+    author: {
+        id: string
+        username: string
+        avatarUrl?: string
+    };
+    createdAt: string;
+    updatedAt: string;
+};
+export interface CreateCommentRequest {
+    content: string;
+};
+
+@Injectable({
+    providedIn: "root",
+})
+export class CommentService {
+    private apiUrl = "http://localhost:8000/api/posts/";
+    constructor(private http: HttpClient) {}
+    getComments(postId: string): Observable<Comment[]> {
+        return this.http.get<Comment[]>(`${this.apiUrl}${postId}/comments`);
+    }
+    createComment(postId: string, data: CreateCommentRequest): Observable<Comment> {
+        return this.http.post<Comment>(`${this.apiUrl}${postId}/comments`, data);
+    }
+    deleteComment(postId: string, commentId: string): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}${postId}/comments/${commentId}`);
+    }
+    updateComment(postId: string, commentId: string, data: CreateCommentRequest): Observable<Comment> {
+        return this.http.put<Comment>(`${this.apiUrl}${postId}/comments/${commentId}`, data);
+    }
+}
