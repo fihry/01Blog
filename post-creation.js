277f6cf2b57f2f168cc66cfc4daea425253f8001
fihry <fihrylara@gmail.com>
2026-01-29 15:59:46 +0100

    feat: add MIT License to the repository

--- post-creation.js
+++
@@ -1,5 +0,0 @@
-const FormData = require('form-data');
-const form = new FormData();
-form.append('post', JSON.stringify({ title: '...', content: '...'}), { contentType: 'application/json' });
-form.append('media', fs.createReadStream('/path/to/image.jpg'));
-fetch('http://localhost:8000/api/posts', { method: 'POST', headers: { Authorization: `Bearer ${token}`, ...form.getHeaders() }, body: form })
